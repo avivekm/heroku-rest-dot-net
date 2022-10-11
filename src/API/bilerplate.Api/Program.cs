1c6f4f46654ff30cfd2630ee0346e8974e5d7a7c
avivekm <vivek.maurya@neosoftmail.com>
2022-10-11 17:10:19 +0530

    second commit

--- src/API/bilerplate.Api/Program.cs
+++ src/API/bilerplate.Api/Program.cs
@@ -66,40 +66,7 @@ services.AddCors(options =>
 services.AddApplicationServices();
 
 services.AddInfrastructureServices(Configuration);
-
-var defaultConnectionString = string.Empty;
-
-if (builder.Environment.EnvironmentName == "Development")
-{
-    defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-}
-else
-{
-    // Use connection string provided at runtime by Heroku.
-    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
-    connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
-    var userPassSide = connectionUrl.Split("@")[0];
-    var hostSide = connectionUrl.Split("@")[1];
-
-    var user = userPassSide.Split(":")[0];
-    var password = userPassSide.Split(":")[1];
-    var host = hostSide.Split("/")[0];
-    var database = hostSide.Split("/")[1].Split("?")[0];
-
-    defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
-}
-
 services.AddPersistenceServices(Configuration);
-var serviceProvider = builder.Services.BuildServiceProvider();
-try
-{
-    var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
-    dbContext.Database.Migrate();
-}
-catch
-{
-}
 services.AddIdentityServices(Configuration);
 services.AddScoped<ILoggedInUserService, LoggedInUserService>();
 services.AddSwaggerExtension();
