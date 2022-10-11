e882371fb1e1172de9838ca7c7b8566e7ad9dd83
avivekm <vivek.maurya@neosoftmail.com>
2022-10-11 17:31:41 +0530

    second commit

--- src/Infrastructure/bilerplate.Identity/IdentityServiceExtensions.cs
+++ src/Infrastructure/bilerplate.Identity/IdentityServiceExtensions.cs
@@ -20,9 +20,40 @@ namespace bilerplate.Identity
         public static void AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+            var defaultConnectionString = string.Empty;
+            var envvar = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            if (envvar == "Development")
+            {
+                defaultConnectionString = configuration.GetConnectionString("ConnectionStrings");
+            }
+            else
+            {
+                // Use connection string provided at runtime by Heroku.
+                var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
+                var userPassSide = connectionUrl.Split("@")[0];
+                var hostSide = connectionUrl.Split("@")[1];
+
+                var user = userPassSide.Split(":")[0];
+                var password = userPassSide.Split(":")[1];
+                var host = hostSide.Split("/")[0];
+                var database = hostSide.Split("/")[1].Split("?")[0];
+
+                defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+            }
             services.AddDbContext<IdentityDbContext>(
                         options => options.UseNpgsql(configuration.GetConnectionString("IdentityConnectionString"),
                         b => b.MigrationsAssembly(typeof(IdentityDbContext).Assembly.FullName)));
+            var serviceProvider = services.BuildServiceProvider();
+            try
+            {
+                var dbContext = serviceProvider.GetRequiredService<IdentityDbContext>();
+                dbContext.Database.Migrate();
+            }
+            catch
+            {
+            }
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<IdentityDbContext>().AddDefaultTokenProviders();
 
