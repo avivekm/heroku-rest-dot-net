5f05763b44b04a2909b30cfe7b2bfe92f1f0f491
avivekm <vivek.maurya@neosoftmail.com>
2022-10-11 17:23:58 +0530

    second commit

--- src/Infrastructure/bilerplate.Persistence/PersistenceServiceRegistration.cs
+++ src/Infrastructure/bilerplate.Persistence/PersistenceServiceRegistration.cs
@@ -11,9 +11,39 @@ namespace bilerplate.Persistence
     {
         public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
+            var defaultConnectionString = string.Empty;
+            var envvar = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            if (envvar == "Development")
+            {
+                defaultConnectionString = configuration.GetConnectionString("ConnectionStrings");
+            }
+            else
+            {
+                // Use connection string provided at runtime by Heroku.
+                var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
+                connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
+                var userPassSide = connectionUrl.Split("@")[0];
+                var hostSide = connectionUrl.Split("@")[1];
+
+                var user = userPassSide.Split(":")[0];
+                var password = userPassSide.Split(":")[1];
+                var host = hostSide.Split("/")[0];
+                var database = hostSide.Split("/")[1].Split("?")[0];
+
+                defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+            }
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseNpgsql(configuration.GetConnectionString("ApplicationConnectionString")));
+            var serviceProvider = services.BuildServiceProvider();
+            try
+            {
+                var dbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+                dbContext.Database.Migrate();
+            }
+            catch
+            {
+            }
             services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IEventRepository, EventRepository>();
